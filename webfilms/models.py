603c3245ee0fc43aca61d80f75553aaf4683fa11
Jakub Wiejak <wiejoit@gmail.com>
2022-02-08 14:31:52 +0100

    morde models

--- webfilms/models.py
+++ webfilms/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Films(models.Model):
+    title = models.CharField(max_length=64, blank=False, unique=True)
+    year = models.PositiveSmallIntegerField(default=2000)
+    description = models.TextField(default='')
+    premiere = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title} ({self.year})'
