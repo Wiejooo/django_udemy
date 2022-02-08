9207533c38b5517681cf147092fff1dba6057e41
Jakub Wiejak <wiejoit@gmail.com>
2022-02-08 22:39:24 +0100

    more films and filter

--- webfilms/admin.py
+++ webfilms/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Films
 
-admin.site.register(Films)
+# admin.site.register(Films)
+
+
+@admin.register(Films)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ['title', 'year']
+    list_filter = ('year', )
+    search_fields = ('title', )
--- webfilms/models.py
+++ webfilms/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+empty_description = 'Chwilowy brak opisu.'
+
 
 class Films(models.Model):
     title = models.CharField(max_length=64, blank=False, unique=True)
     year = models.PositiveSmallIntegerField(default=2000)
-    description = models.TextField(default='')
+    description = models.TextField(default=empty_description, blank=True)
     premiere = models.DateField(null=True, blank=True)
+    imdb_rating = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    picture = models.ImageField(upload_to='picture', null=True, blank=True)
 
     def __str__(self):
         return f'{self.title} ({self.year})'
+
